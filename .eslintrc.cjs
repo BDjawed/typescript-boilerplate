e1fdcf0d1e51d9a71981157f168e893696015eb8
Djawed Boutiba <djawed.boutiba@gmail.com>
2024-01-26 08:26:36 +0100

    modified:   .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,58 +1,43 @@
 module.exports = {
-	"env": {
-		"browser": true,
-		"es2021": true,
-		"node": true
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
 	},
-	"extends": [
+	extends: [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
 		"prettier",
 		"plugin:prettier/recommended",
-    	"plugin:import/recommended"
+		"plugin:import/recommended",
 	],
-	"overrides": [
+	overrides: [
 		{
-			"env": {
-				"node": true
+			env: {
+				node: true,
 			},
-			"files": [
-				".eslintrc.{js,cjs,ts,mts,cts,jsx,tsx}"
-			],
-			"parserOptions": {
-				"sourceType": "script"
-			}
-		}
+			files: [".eslintrc.{js,cjs,ts,mts,cts,jsx,tsx}"],
+			parserOptions: {
+				sourceType: "script",
+			},
+		},
 	],
-	"parser": "@typescript-eslint/parser",
-	"parserOptions": {
-		"ecmaVersion": "latest",
-		"sourceType": "module",
-		"project": "./tsconfig.json"
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+		project: "./tsconfig.json",
+		extraFileExtensions: [".json", ".yaml"],
 	},
-	"plugins": [
-		"@typescript-eslint"
-	],
-	"rules": {
-		"indent": [
-			"error",
-			"tab"
-		],
-		"linebreak-style": [
-			"error",
-			"unix"
-		],
-		"quotes": [
-			"error",
-			"double"
-		],
-		"semi": [
-			"error",
-			"always"
-		],
+	plugins: ["@typescript-eslint"],
+	rules: {
+		indent: ["error", "tab"],
+		"linebreak-style": ["error", "windows"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
 		"@typescript-eslint/no-unused-vars": "error",
 		"@typescript-eslint/consistent-type-definitions": ["error", "type"],
-		"@typescript-eslint/no-use-before-define": "error"
+		"@typescript-eslint/no-use-before-define": "error",
 	},
-	"ignorePatterns": ["dist"]
+	ignorePatterns: ["./dist", "./node_modules", "./pnpm-lock.yaml"],
 };
